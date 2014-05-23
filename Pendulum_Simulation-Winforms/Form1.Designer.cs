255ee0fecaef8eff578f719559f9e6e060e922cd
Harley Mcphee <harley.1011@gmail.com>
2014-05-22 23:25:08 -0600

    base pendulum

---
+++ Pendulum_Simulation-Winforms/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblAngle = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(693, 298);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(22, 429);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(19, 333);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(17, 13);
+            this.lblX.TabIndex = 4;
+            this.lblX.Text = "X:";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(19, 353);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(17, 13);
+            this.lblY.TabIndex = 5;
+            this.lblY.Text = "Y:";
+            // 
+            // lblAngle
+            // 
+            this.lblAngle.AutoSize = true;
+            this.lblAngle.Location = new System.Drawing.Point(19, 373);
+            this.lblAngle.Name = "lblAngle";
+            this.lblAngle.Size = new System.Drawing.Size(37, 13);
+            this.lblAngle.TabIndex = 6;
+            this.lblAngle.Text = "Angle:";
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(721, 10);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(459, 300);
+            this.chart.TabIndex = 7;
+            this.chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 464);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.lblAngle);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblAngle;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ Pendulum_Simulation-Winforms/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public int circleDiamater = 40;
+        public double angle = 0;
+        public double x = 0;
+        public double y = 0;
+        public double direction = -.5;
+        public int pendulumShaftLength = 200;
+        public Stopwatch stopWatch = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            convertPolarToCartesian(angle);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            GraphicsPath myPath = new GraphicsPath();
+            myPath.AddLine(pictureBox1.Width / 2, 0, (int)x , (int)y );
+            Rectangle pathRect = new Rectangle((int)(x - circleDiamater / 2), (int)(y - circleDiamater / 2), circleDiamater, circleDiamater);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            Pen myPen = new Pen(Color.Black, 2);
+            e.Graphics.DrawPath(myPen, myPath);
+            e.Graphics.FillEllipse(blueBrush, pathRect);
+
+        }
+        public void convertPolarToCartesian(double angle)
+        {
+            x = Math.Cos(angle * Math.PI / 180) * pendulumShaftLength + pictureBox1.Width / 2;       
+            y = -1 * Math.Sin(angle * Math.PI / 180) * pendulumShaftLength;
+           
+        }
+        public void setLabels()
+        {
+            lblX.Text = "X: " + ((int)x).ToString();
+            lblY.Text = "Y: " + ((int)y).ToString();
+            lblAngle.Text = "Angle: " + ((int)angle).ToString() + '°';
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+            timer1.Enabled = true;
+
+            chart.Series.Clear(); //ensure that the chart is empty
+            chart.Series.Add("Series0");
+            chart.Series[0].ChartType = SeriesChartType.Line;
+            chart.Legends.Clear();
+            stopWatch.Start();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (angle <= -180) 
+                direction = 1;
+            else if (angle >= 0)
+                direction = -1;
+            angle += direction;
+            convertPolarToCartesian(angle);
+            setLabels();
+            pictureBox1.Invalidate();
+            chart.Series[0].Points.AddXY(stopWatch.ElapsedMilliseconds, angle); 
+        }
+    }
+}
